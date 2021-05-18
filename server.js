9402366e4c96408c6b5b87fccdad4e7d77c3cb78
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-18 18:27:42 +0530

    2nd commit

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
     var n1 =Number(req.body.num1);
     var n2 = Number(req.body.num2);
-    var result = n1+n2;
-    res.send("Sum is "+result);
+    var result = (n1/(n2*n2))*10000;
+    if(result<18.5){
+        res.send("Your BMI is "+result.toFixed(2)+", You are underweight")
+    }else if(result<24.9){
+        res.send("Your BMI is "+result.toFixed(2)+", You've got a normal BMI!")
+    }else if(result<29.9){
+        res.send("Your BMI is "+result.toFixed(2)+", You are overweight!")
+    }else{
+        res.send("Your BMI is "+result.toFixed(2)+", You are obese!")
+    }
+    
     console.log(req.body.num1);
 });
 
@@ -19,16 +28,3 @@ app.listen(3000,function(){
     console.log("Server has started on port 3000");
 });
 
-/*
-npm init -> to create package
-npm install express
-
-Starting server
-node server.js
-///////////
-npm install -g nodemon
-
-starting server
-nodemon server.js
-
-*/
